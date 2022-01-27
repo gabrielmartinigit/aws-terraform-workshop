d60e84f015d592e598371aee7b129fa84aec3629
Gabriel Martini <gabrielb.martini@gmail.com>
2022-01-27 08:47:31 -0300

    feat(Day2) Day2 labs

--- dia2/lab2/providers.tf
+++ dia2/lab2/providers.tf
@@ -2,9 +2,10 @@
 #### Provider do Terraform ####
 ###############################
 provider "aws" {
-  region                  = "us-east-1"
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "default"
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::SUA_CONTA:role/terraform-prod-role"
+  }
 
   default_tags {
     tags = {
